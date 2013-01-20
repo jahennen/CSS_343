da22e0daca4cd83bc9c128b8e3f793591bca2756
Jay Hennen <jahennen28@hotmail.com>
2013-01-20 10:02:54 -0800

    new hw

---
+++ HW2/src/BTree.cpp
@@ -0,0 +1,18 @@
+/*
+ * BTree.cpp
+ *
+ *  Created on: Jan 20, 2013
+ *      Author: jay
+ */
+
+#include "BTree.h"
+
+BTree::BTree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BTree::~BTree() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ HW2/src/BTree.h
@@ -0,0 +1,17 @@
+/*
+ * BTree.h
+ *
+ *  Created on: Jan 20, 2013
+ *      Author: jay
+ */
+
+#ifndef BTREE_H_
+#define BTREE_H_
+
+class BTree {
+public:
+	BTree();
+	virtual ~BTree();
+};
+
+#endif /* BTREE_H_ */

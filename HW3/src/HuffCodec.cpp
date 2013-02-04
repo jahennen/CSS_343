9bed666927a2730251b0410c0b9cbfc6cb6281c3
Jay Hennen <jahennen28@hotmail.com>
2013-02-03 23:47:05 -0800

    ugh

--- HW3/src/HuffCodec.cpp
+++ HW3/src/HuffCodec.cpp
@@ -3,32 +3,38 @@
 #include "CharFreqCounter.h"
 #include "CounterToHuffNodePQ.h"
 #include <iostream>
+#include <fstream>
 #include <map>
 
 using namespace std;
 
-void preorderPrint(HuffNode * current, string encoding);
-
-int main() {
-	CharFreqCounter counts;
+int main(int argc, char* argv[]) {
+	BTreeMap<char, int> counts;
+	vector<char> types;
 	string line;
-	while (getline(cin,line)) {
+	ifstream file;
+	file.open(argv[1], ios::in);
+	while (getline(file,line)) {
 		unsigned int i;
 		for (i = 0 ; i < line.size() ; i++) {
 			char c = line[i];
-			if (!counts.lookup(c)) {
-				counts.addCount(c);
+			if (counts.containsKey(c)) {
+				int * count = counts.get(c);
+				*count += 1;
 			} else {
-				counts.increment(c);
+				int i = 1;
+				counts.add(c,i);
+				types.push_back(c);
 			}
 		}
 	}
-	counts.print();
 
 	PQueue<HuffNode> queue;
-	// Use conversion object to transform CharFreqCounter elements into HuffNodes,
-	// and i1nsert them into the queue
-	CounterToHuffNodePQ converter(&counts, &queue);
+	while(types.size() > 0) {
+		HuffNode newNode(counts.get(types.back()),types.back());
+		types.pop_back();
+		queue.push(newNode);
+	}
 
 	while (queue.size() > 1) {
 		HuffNode * node1 = new HuffNode(queue.pop());
@@ -38,17 +44,9 @@ int main() {
 	}
 	HuffNode root = queue.pop();
 
-	map<char, string> encodings;
+	BTreeMap<char, string> encodings;
+
+
 
 	return 0;
-}
-
-void preorderPrint(HuffNode * current) {
-	if(current) {
-		if ( current->c != '\0') {
-			cout << current->c << " " << current->count<< endl;
-		}
-		preorderPrint(current->left);
-		preorderPrint(current->right);
-	}
 }

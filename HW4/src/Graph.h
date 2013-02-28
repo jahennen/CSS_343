651b1fa86abcce8f4df32dbb9e61a2030d4c05ef
Jay Hennen <jahennen28@hotmail.com>
2013-02-27 21:47:11 -0800

    derp

--- HW4/src/Graph.h
+++ HW4/src/Graph.h
@@ -10,7 +10,6 @@
 
 #include <list>
 #include <vector>
-#include <cstdlib>
 
 class Graph {
 public:
@@ -20,6 +19,10 @@ public:
 			id_(id), label_(label), cost_(cost) {}
 		const std::string& getID() {return id_;}
 		const std::string& getLabel() {return label_;}
+		std::string toString() {
+			return id_ + " " + label_ + " " + cost_;
+		}
+		friend bool operator<(const Node&, const Node&);
 	private:
 		std::string id_;
 		std::string label_;
@@ -29,15 +32,14 @@ public:
 	class Edge {
 	public:
 		Edge(Node & from, Node & to, int w):
-		from_(from), to_(to), w_(w){
-			if(!from_ || !to_ || w_ < 0) {
-				std::cout << "Illegal arguments to Edge constructor\n";
-				exit (EXIT_FAILURE);
-			}
-		};
+		from_(from), to_(to), w_(w){};
 		const Node& from() {return from_;}
 		const Node& to() {return to_;}
 		const int label() {return w_;}
+		std::string toString() {
+			return from_ + " " + to_ + " " + w_;
+		}
+		friend bool operator<(const Edge&, const Edge&);
 	private:
 		Node * from_;
 		Node * to_;
@@ -45,10 +47,10 @@ public:
 	};
 	Graph(std::list<std::string> & nodes, std::vector<std::string> & edges);
 	virtual ~Graph();
- 	void getPath(std::vector<std::string> & path, std::string & n1, std::string & n2);
+	void getPath(std::vector<std::string> & path, std::string & n1, std::string & n2);
 private:
-	std::list<Node> nodes;
-	std::list<Edge> edges;
+	std::list<Node*> nodes;
+	std::list<Edge*> edges;
 };
 
 #endif /* GRAPH_H_ */
